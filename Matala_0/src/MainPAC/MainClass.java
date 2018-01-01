827ce910eb1667f0a567f91bde65fd7ccba9a28f
Tal <Tal@DESKTOP-S30F9EQ>
2018-01-01 19:21:28 +0200

    Update MainPAC Package

---
+++ Matala_0/src/MainPAC/MainClass.java
@@ -0,0 +1,30 @@
+package MainPAC;
+
+import ReadAndWriteFiles.ReadWigleWifiFiles;
+
+public class MainClass {
+
+	
+	String folderPath="";
+
+	
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+	//	if ()
+		//csvToNewCsv chek = new csvToNewCsv("C:\\matala\\chek");
+
+		
+		
+	}
+	
+	public static void csvTOcomb(String path)
+	{
+		//csvToNewCsv chek = new csvToNewCsv("C:\\matala\\chek");
+
+	}
+	
+	
+	
+	
+}
