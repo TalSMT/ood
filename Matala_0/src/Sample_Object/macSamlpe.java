928baf848103be2da2c8e341211cd07aa1aa9811
Tal <Tal@DESKTOP-S30F9EQ>
2018-01-01 19:31:24 +0200

    Update Sample_Object Package

--- Matala_0/src/Sample_Object/macSamlpe.java
+++ Matala_0/src/Sample_Object/macSamlpe.java
@@ -1,33 +1,19 @@
 package Sample_Object;
-/**
- * 
- * @authors Tal And Shaked
- * This class contains objects for sampling by MAC address
- *
- */
+
 public class macSamlpe {
-	/***
-	 * setting
-	 */
 	private double signal;
 	private double lat;
 	private double lon;
 	private double alt;
+	//
 	private String mac;
 	//for algo 2
 	private double diff;
 	private double weight;
 	private double piWeight;
-	private double Average_diff;
 
-	/**
-	 * 
-	 * @param signal
-	 * @param lat
-	 * @param lon
-	 * @param alt
-	 * First constructor for the first algo
-	 */
+
+	//First constructor for the first algo
 	public macSamlpe(double signal, double  lat, double lon, double alt) {
 		this.signal=signal;
 		this.lat= lat;
@@ -35,37 +21,7 @@ public class macSamlpe {
 		this.alt=alt;
 
 	}			
-/**
- * for A function that removes duplicate values using HashSet
- */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((mac == null) ? 0 : mac.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		macSamlpe other = (macSamlpe) obj;
-		if (mac == null) {
-			if (other.mac != null)
-				return false;
-		} else if (!mac.equals(other.mac))
-			return false;
-		return true;
-	}
-/**
- * 
- * @return Getters And Setters
- */
 	public String getMac() {
 		return mac;
 	}
@@ -140,12 +96,6 @@ public class macSamlpe {
 
 	public void setAlt(int alt) {
 		this.alt = alt;
-	}
-	public double getAverage_diff() {
-		return Average_diff;
-	}
-	public void setAverage_diff(double average_diff) {
-		Average_diff = average_diff;
 	}
 
 }
