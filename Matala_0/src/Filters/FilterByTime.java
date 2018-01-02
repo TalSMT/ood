99b902d6f1cdf51a7cf7b5395654cf000d94f613
Tal <Tal@DESKTOP-S30F9EQ>
2018-01-02 11:32:09 +0200

    Correction filter by time, and the creation of system design diagram

--- Matala_0/src/Filters/FilterByTime.java
+++ Matala_0/src/Filters/FilterByTime.java
@@ -8,7 +8,6 @@ import Sample_Object.SampleOfWifi;
 
 
 import java.util.Date;
-import java.io.FileNotFoundException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
@@ -18,55 +17,62 @@ import java.time.format.*;
 import java.util.*;
 
 /**
- * 
- * @authors Tal And Shaked
  * This class filters by Time
+ * @authors Tal And Shaked
  *
  */
 public class FilterByTime implements Condition {
-	private LocalTime timeMIN;
-	private LocalTime timeMAX;
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);//ìùðåú ìôåøîè ùìðå
-	private DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.US);//ìùðåú ìôåøîè ùìðå
-
+//	private LocalTime timeMIN;
+//	private LocalTime timeMAX;
+//	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);//לשנות לפורמט שלנו
+//	private DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.US);//לשנות לפורמט שלנו
+    private String startTime;
+    private String endTime;
 
 	/**
 	 *
 	 * @param phoneIdFilter
 	 */
-	public FilterByTime(String timeMINstr, String timeMAXstr) {//https://stackoverflow.com/questions/29927362/how-to-convert-date-to-localtime
-		super();
-		long hthala = Date.parse(timeMINstr);// option 1 - not working
-		long sof = Date.parse(timeMAXstr);
-		
-		System.out.println(timeMINstr);
-        LocalDateTime localDateTimeMIN = LocalDateTime.parse(timeMINstr, formatter); // option 2 not working as well
-        this.timeMIN= localDateTimeMIN.toLocalTime();
-        
-        LocalDateTime localDateTimeMAX = LocalDateTime.parse(timeMAXstr, formatter);
-        this.timeMAX= localDateTimeMAX.toLocalTime();
+//	public FilterByTime(String timeMINstr, String timeMAXstr) {//https://stackoverflow.com/questions/29927362/how-to-convert-date-to-localtime
+//		super();
+//		System.out.println(timeMINstr);
+//        LocalDateTime localDateTimeMIN = LocalDateTime.parse(timeMINstr, formatter);
+//        this.timeMIN= localDateTimeMIN.toLocalTime();
+//        
+//        LocalDateTime localDateTimeMAX = LocalDateTime.parse(timeMAXstr, formatter);
+//        this.timeMAX= localDateTimeMAX.toLocalTime();
 
+//	}
+
+	public FilterByTime(String startTime, String endTime){
+		this.startTime=startTime;
+		this.endTime=endTime;
 	}
-
-	
 	
 	
 
 	@Override
+//	public boolean test(SampleOfWifi s) {
+//		  LocalDateTime localDateTimeS = LocalDateTime.parse(s.getTime(), formatter);
+//		  LocalTime stime= localDateTimeS.toLocalTime();
+//		  
+//		return (stime.isAfter(this.timeMIN)&&stime.isBefore(this.timeMAX));
+//	}
+
 	public boolean test(SampleOfWifi s) {
-		  LocalDateTime localDateTimeS = LocalDateTime.parse(s.getTime(), formatter);
-		  LocalTime stime= localDateTimeS.toLocalTime();
-		  
-		return (stime.isAfter(this.timeMIN)&&stime.isBefore(this.timeMAX));
+		String time= s.getTime();
+		int sTime=startTime.compareTo(time);
+		int eTime=endTime.compareTo(time);
+		boolean ans=false;
+		if(s!=null){
+			if(sTime<=0 && eTime>0){
+				ans =true;
+			}
+			
+		}
+		return ans;
 	}
-
 	
-	//
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		FilterByTime filterTIME=new FilterByTime("28/10/2017 20:10:00","28/10/2017 20:30:00");
-		long d = Date.parse("28/10/2017 20:10:00");
-
-	}
+	
 	
 }
