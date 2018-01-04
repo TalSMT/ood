25161d4b520b5dddcf1fbca283e225d57386f2f2
Tal <Tal@192.168.14.36>
2018-01-04 20:29:08 +0200

    Update JavaDoc and Folders have been ordered

--- Matala_0/src/Filters/FilterByTime.java
+++ Matala_0/src/Filters/FilterByTime.java
@@ -17,33 +17,15 @@ import java.time.format.*;
 import java.util.*;
 
 /**
- * This class filters by Time
+ * This class filters time. The filter receives minimal time and maximum time and returns all dates between them
  * @authors Tal And Shaked
  *
  */
 public class FilterByTime implements Condition {
-//	private LocalTime timeMIN;
-//	private LocalTime timeMAX;
-//	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);//לשנות לפורמט שלנו
-//	private DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", Locale.US);//לשנות לפורמט שלנו
-    private String startTime;
+   private String startTime;
     private String endTime;
 
-	/**
-	 *
-	 * @param phoneIdFilter
-	 */
-//	public FilterByTime(String timeMINstr, String timeMAXstr) {//https://stackoverflow.com/questions/29927362/how-to-convert-date-to-localtime
-//		super();
-//		System.out.println(timeMINstr);
-//        LocalDateTime localDateTimeMIN = LocalDateTime.parse(timeMINstr, formatter);
-//        this.timeMIN= localDateTimeMIN.toLocalTime();
-//        
-//        LocalDateTime localDateTimeMAX = LocalDateTime.parse(timeMAXstr, formatter);
-//        this.timeMAX= localDateTimeMAX.toLocalTime();
-
-//	}
-
+	
 	public FilterByTime(String startTime, String endTime){
 		this.startTime=startTime;
 		this.endTime=endTime;
@@ -52,13 +34,6 @@ public class FilterByTime implements Condition {
 	
 
 	@Override
-//	public boolean test(SampleOfWifi s) {
-//		  LocalDateTime localDateTimeS = LocalDateTime.parse(s.getTime(), formatter);
-//		  LocalTime stime= localDateTimeS.toLocalTime();
-//		  
-//		return (stime.isAfter(this.timeMIN)&&stime.isBefore(this.timeMAX));
-//	}
-
 	public boolean test(SampleOfWifi s) {
 		String time= s.getTime();
 		int sTime=startTime.compareTo(time);
