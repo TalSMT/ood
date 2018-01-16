13d3a271b8cadb5ad1b0eefaa0bfccf245709aa7
Tal <Tal@192.168.14.36>
2018-01-16 13:00:21 +0200

    Add Sql Window to the git

--- Matala_0/src/Gui_Designer/SQL_window.java
+++ Matala_0/src/Gui_Designer/SQL_window.java
@@ -12,7 +12,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+////
 public class SQL_window {
 
 	public static JFrame frame;
